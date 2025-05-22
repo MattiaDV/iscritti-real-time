60ac046202e03c8abaa51b265d44758ad05d16bb
MattiaDV <mattiadv25105@gmail.com>
2025-05-22 17:48:37 +0200

    a

--- follower_count.js
+++ follower_count.js
@@ -1,3 +1,8 @@
 export let iscritti_youtube = 94;
-export let iscritti_tiktok = 395;
+export let iscritti_tiktok = 396;
 export let iscritti_instagram = 1616;
+
+// PARTENZE
+// Youtube 80
+// TikTok 395
+// Instagram 1616
