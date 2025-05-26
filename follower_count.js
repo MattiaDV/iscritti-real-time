0aa67e96477c8cb38a84dffb9761ac1333446069
MattiaDV <79549154+MattiaDV@users.noreply.github.com>
2025-05-26 20:26:31 +0200

    Update follower_count.js

--- follower_count.js
+++ follower_count.js
@@ -1,5 +1,5 @@
 export let iscritti_tiktok = 404;
-export let iscritti_instagram = 1627;
+export let iscritti_instagram = 1626;
 
 const youtubeChannelID = "UCS7kt-sPzn--kLM-f_TVPGw";
 const url = `https://api.socialcounts.org/youtube-live-subscriber-count/${youtubeChannelID}`;
